45024b8f042dbb32d88b0eda98a5ef19c6b0bd32
Randy_Lin <randy_lin@mail.chungyo.net>
2022-08-11 14:55:19 +0800

    init

---
+++ dist/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get(/js|css|img/, (req, res) => {
+  res.sendFile(`${__dirname}/${req.path}`);
+});
+
+app.get(/\//, (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.listen(port);
